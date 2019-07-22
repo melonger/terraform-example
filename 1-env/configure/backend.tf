46722713dd1b89e325d86aa56ee09c39e362fb15
melonger <19414334+melonger@users.noreply.github.com>
2019-07-22 10:35:02 -0400

    Delete backend.tf

--- 1-env/configure/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "azurerm" {
-    key                  = "DOUEAMPLE/qa/config/terraform.tfstate"
-    storage_account_name = "digitalonusstorage"
-    container_name       = "terraform-state"
-  }
-}
