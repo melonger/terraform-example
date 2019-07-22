b044a980d3b827cb69cf73f64a9e0e26186ccd09
melonger <19414334+melonger@users.noreply.github.com>
2019-07-22 10:34:27 -0400

    Delete backend.tf

--- 1-env/publish/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "azurerm" {
-    key                  = "DOUEXAMPLE/qa/publish/terraform.tfstate"
-    storage_account_name = "digitalonusstorage"
-    container_name       = "terraform-state"
-  }
-}
